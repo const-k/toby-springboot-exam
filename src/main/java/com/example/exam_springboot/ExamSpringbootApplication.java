945e8cfeb5b286cb7075136a97b9bb7bf25e24e4
const-k <kss07120@gmail.com>
2023-02-21 11:11:08 +0900

    @Configuration 사용하도록 수정

--- src/main/java/com/example/exam_springboot/ExamSpringbootApplication.java
+++ src/main/java/com/example/exam_springboot/ExamSpringbootApplication.java
@@ -3,14 +3,29 @@ package com.example.exam_springboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 //@SpringBootApplication
+@Configuration // 자바 코드로 된 구성 정보임을 스프링에 알려주기 위해 사용
 public class ExamSpringbootApplication {
 
+    // helloController = factory 메소드, 스프링 컨테이너가 빈 오브젝트를 만들기 위해 사용되는 팩토리 메소드로 인식되도록 @Bean 사용
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
-        final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        final AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -25,8 +40,7 @@ public class ExamSpringbootApplication {
             }
         };
 
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(ExamSpringbootApplication.class);
         // 컨테이너 초기화 -> applicationContext 가 빈을 모두 생성함
         applicationContext.refresh();
     }
