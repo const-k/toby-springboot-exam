56129766d025c32c4b44e66a69845d726ef2ecdd
const-k <kss07120@gmail.com>
2023-02-22 11:02:06 +0900

    tomcat, dispatcherServlet 빈으로 수정

--- src/main/java/com/example/exam_springboot/ExamSpringbootApplication.java
+++ src/main/java/com/example/exam_springboot/ExamSpringbootApplication.java
@@ -3,6 +3,7 @@ package com.example.exam_springboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,16 +13,28 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration // 자바 코드로 된 구성 정보임을 스프링에 알려주기 위해 사용
 @ComponentScan
 public class ExamSpringbootApplication {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
     public static void main(String[] args) {
         final AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
 
-                final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                final ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                final DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+//                dispatcherServlet.setApplicationContext(this); // bean lifecycle 메서드에 의해 자동으로 applicationContext가 주입됨
+
                 final WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet",
-                                              new DispatcherServlet(this))
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
                         .addMapping("/*");
                 });
                 webServer.start();
--- src/main/java/com/example/exam_springboot/HelloController.java
+++ src/main/java/com/example/exam_springboot/HelloController.java
@@ -1,19 +1,32 @@
 package com.example.exam_springboot;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 public class HelloController {
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    // 필요한 bean은 생성자를 통해 받음 -> 스프링 컨테이너가 알아서 스캔 후 주입해줌
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        log.info("applicationContext={}", applicationContext);
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(name);
     }
+
+//    // ApplicationContextAware 구현, 스프링 컨테이너 초기화되는 시점에 호출됨
+//    @Override
+//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+//        log.info("applicationContext={}", applicationContext);
+//    }
 }
