6134f123d416ecc8e63b66a93912aeda3d1e3543
const-k <kss07120@gmail.com>
2023-02-25 13:27:57 +0900

    custom unit test 애노테이션 추가

--- src/test/java/com/example/exam_springboot/HelloServiceTest.java
+++ src/test/java/com/example/exam_springboot/HelloServiceTest.java
@@ -3,10 +3,28 @@ package com.example.exam_springboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloServiceTest {
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    @UnitTest
+    @interface FastUnitTest {
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+    @Test
+    @interface UnitTest {
+    }
+
     @Test
     void simpleHelloService() {
         final SimpleHelloService helloService = new SimpleHelloService();
