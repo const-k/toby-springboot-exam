6eaec318a3612a8b8b617775483f37232bcc0198
const-k <kss07120@gmail.com>
2023-02-25 10:59:15 +0900

    TestRestTemplate 이용한 테스트 코드 생성

---
+++ src/test/java/com/example/exam_springboot/HelloApiTest.java
@@ -0,0 +1,24 @@
+package com.example.exam_springboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class HelloApiTest {
+    @Test
+    void helloApi() {
+        final TestRestTemplate rest = new TestRestTemplate();
+        final ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        assertThat(res.getBody()).isEqualTo("Hello Spring");
+
+    }
+}
