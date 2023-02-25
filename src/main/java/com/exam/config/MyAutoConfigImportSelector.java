96d8e8e94fb85bffa17502ffaa17bf0d220e4d01
const-k <kss07120@gmail.com>
2023-02-25 17:34:27 +0900

    자동 구성 정보 파일 분리

--- src/main/java/com/exam/config/MyAutoConfigImportSelector.java
+++ src/main/java/com/exam/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,28 @@
 package com.exam.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-            "com.exam.config.autoconfig.DispatcherServletConfig",
-            "com.exam.config.autoconfig.TomcatWebServerConfig"
-        };
+        // candidates = 파일에다 설정해둘 자동구성에 사용할  configuration 클래스의 목록들이 들어감
+        final List<String> autoConfigs = new ArrayList<>();
+
+        // META-INF/spring 위치의 full-qualified-annotation-name.imports 파일을 자동구성에 사용
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader)
+            .forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
     }
 }
