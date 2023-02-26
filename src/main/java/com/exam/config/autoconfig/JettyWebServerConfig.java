88b4f463ed39cd14d5deae08b9ec316b5131e4ef
const-k <kss07120@gmail.com>
2023-02-26 13:58:30 +0900

    jetty container 추가

--- src/main/java/com/exam/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/exam/config/autoconfig/JettyWebServerConfig.java
@@ -1,15 +1,15 @@
 package com.exam.config.autoconfig;
 
 import com.exam.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 
 }
