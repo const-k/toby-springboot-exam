a2eef251cbb272419cff49b43e7b968f71a60410
const-k <kss07120@gmail.com>
2023-02-27 16:23:35 +0900

    사용자 구성 정보로 자동 구성 정보 대체

--- src/main/java/com/exam/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/exam/config/autoconfig/JettyWebServerConfig.java
@@ -2,19 +2,16 @@ package com.exam.config.autoconfig;
 
 import com.exam.config.ConditionalMyOnClass;
 import com.exam.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
--- src/main/java/com/exam/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/exam/config/autoconfig/TomcatWebServerConfig.java
@@ -2,14 +2,16 @@ package com.exam.config.autoconfig;
 
 import com.exam.config.ConditionalMyOnClass;
 import com.exam.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat") // 클래스가 존재하는지, 라이브러리가 존재하는지 체크
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean // 이미 등록된 ServletWebServerFactory 빈이 없다면 빈 생성, 유저 구성정보 빈을 먼저 등록 후 자동 구성 정보 빈을 등록함
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
